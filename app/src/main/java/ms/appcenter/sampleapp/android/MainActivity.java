23f25391c029d7d39e3131b488a778ffa3f88b28
bakrish <bakrish@microsoft.com>
2023-03-09 09:58:47 +0530

    App secret

--- app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
+++ app/src/main/java/ms/appcenter/sampleapp/android/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     Analytics.class, Crashes.class, Distribute.class);
         } else {
             // Otherwise use the hardcoded string value here
-            AppCenter.start(getApplication(), "<APP SECRET HERE>",
+            AppCenter.start(getApplication(), "aefa9306-841a-400b-be31-14d061bc3a7a",
                     Analytics.class, Crashes.class, Distribute.class);
         }
 
